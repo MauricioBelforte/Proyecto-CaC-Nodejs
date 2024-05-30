a51a8b0c8cba9ce284c5031f419ce21cbf8095dd
Mauricio Belforte <belymau@hotmail.com>
2024-05-29 23:27:14 -0300

    Se agrego que se esconda el menu desplegable cuando se hace click en los enlaces a del menu variable

--- assets/js/menus.js
+++ assets/js/menus.js
@@ -2,12 +2,26 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.boton-hamburguesa');
     const mainMenu = document.querySelector('.menu-variable');
     const botonesInicio = document.querySelector('.botones-iniciosesion-registro');
+    const menuLinks = document.querySelectorAll('.menu-variable a'); // Selecciona todos los enlaces dentro del menú
 
     menuToggle.addEventListener('click', function() {
         mainMenu.classList.toggle('show');  /* classList despliega todas las claeses del objeto y la propiedad toggle agrega la clase si no esta, y si esta la quita */
         botonesInicio.classList.toggle('show');
     });
+
+   
+    // Añadir un evento click a cada enlace del menú para cerrar el menú al hacer clic
+    menuLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            mainMenu.classList.remove('show');
+            botonesInicio.classList.remove('show');
+        });
+    });
+
 });
+
+
+
 
 
 
